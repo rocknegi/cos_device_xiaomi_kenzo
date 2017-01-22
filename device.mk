9b310e0747ad38c5d10e9575d3a39644888524a2
TheScarastic <warabhishek@gmail.com>
2017-01-22 06:06:43 +0000

    kenzo: Import oss camera hal from caf
     * LA.BR.1.3.3_rb2.26
    
    Change-Id: I327b443ce8276cf0a3fe785ea3338b4a8bcc097a
    
    kenzo: Import oss camera hal from caf

--- device.mk
+++ device.mk
@@ -25,6 +25,31 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_wcd9326.xml
 
+<<<<<<< HEAD
+=======
+# Alipay / WeChat
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manage
+
+# init.d
+# BSOD Killer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/etc/init.d/89bsod:system/etc/init.d/89bsod
+
+
+# Flashlight
+PRODUCT_PACKAGES += \
+    Flashlight
+
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8952 \
+    libqomx_core \
+    libmm-qcamera
+
+
+>>>>>>> 091b450... kenzo: Import oss camera hal from caf
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
